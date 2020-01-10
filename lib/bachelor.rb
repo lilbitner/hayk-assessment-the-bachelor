09d9349bb54d3663e3f032642af9db1652264434
github username <github email address>
2020-01-10 15:31:02 -0700

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,49 +1,59 @@
+require 'json'
+require 'pry'
+#contestants = JSON.parse(File.read('spec/fixtures/contestants.json'))
 
-contestants = JSON.parse(File.read('spec/fixtures/contestants.json'))
-
-
-def get_first_name_of_season_winner(data, season)
-  contestants.each do |season_number, contestant_data|
-    if season == season_number 
-    contestant_data.each do |specific_contestant| 
+def get_first_name_of_season_winner(bachelortv, season)
+  bachelortv.each do |season_number, contestant_data|
+    if season == season_number
+    contestant_data.each do |specific_contestant|
      if specific_contestant["status"] == "Winner"
-     new_array = specific_contestant["name"].split 
-    return new_array[0] 
-        end  
+     new_array = specific_contestant["name"].split
+    return new_array[0]
+        end
       end
-    end     
+    end
   end
 end
-
-# def get_contestant_name(data, occupation)
-#   # code here
-#   contestants.each do |contestant_data, contestant_occupation| 
-#     data.each do |contestant_data|
-#       contestant_data.each do |contestant_occupation| 
-#         if contestant_occupation == occupation 
-#           contestant_data.each do |"name"|
-#       return "name" 
-#     end 
-#   end 
-# end
-
-# def count_contestants_by_hometown(data, hometown)
-#   contestants.each do |contestant_data, contestant_hometown|
-#     data.each do |contestant_data|
-#       contestant_data.each do |contestant_hometown|
-#       if contestant_hometown == hometown
-#         puts contestant_hometown[count]
-#       count += 1 
-#       end
-# end
-
-# def get_occupation(data, hometown)
-#   contestants.each do |contestant_data, contestant_ocupation|
-#     if contestant_data.find[:hometown] == hometown 
-#     contestant_data[:occupation] == contestant_occupation 
-#     contestant_occupation 
-# end
-
+def contestants(bachelortv)
+  bachelortv.values.flatten
+end
+def get_contestant_name(bachelortv, occupation)
+  match = contestants(bachelortv).find do |contestant|
+    contestant["occupation"] == occupation
+  end
+match["name"]
+end
+def count_contestants_by_hometown(bachelortv, hometown)
+  contestants(bachelortv).count(hometown)
+    if contestant["hometown"] == hometown
+  end
+end
+def get_occupation(bachelortv, hometown)
+  match = contestants(bachelortv).find do |contestant|
+    contestant["hometown"] = hometown
+  end
+match["occupation"]
+end
 # def get_average_age_for_season(data, season)
-#   # code here
+#   bachelortv.each do |season_number, contestant_data|
+#     if season == season_number
+#   contestant_data.each do |contestant|
+#     age_value = contestant["age"]
+#     age_value.to_f
+#     total_age_value = age_value.sum
+#     total_age_value/(contestant.length)
+#       end
+#     end
+#   end
 # end
+def get_average_age_for_season(bachelortv, season)
+  bachelortv.each do |season_number, contestant_data|
+    if season == season_number
+      contestant_data.each do |contestant|
+        contestant["age"].reduce(0) do |sum|
+          sum += age
+        end
+      end
+    end
+  end
+end
