e1e70ab2bd7fc30496ff22b92a015ec42b93bbcc
github username <github email address>
2020-01-10 15:49:02 -0700

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -14,26 +14,32 @@ def get_first_name_of_season_winner(bachelortv, season)
     end
   end
 end
+
 def contestants(bachelortv)
   bachelortv.values.flatten
 end
+
 def get_contestant_name(bachelortv, occupation)
   match = contestants(bachelortv).find do |contestant|
     contestant["occupation"] == occupation
   end
 match["name"]
 end
+
 def count_contestants_by_hometown(bachelortv, hometown)
-  contestants(bachelortv).count(hometown)
-    if contestant["hometown"] == hometown
+  contestants(bachelortv).select do |contestant|
+    contestant["hometown"] == hometown 
   end
+  .count()
 end
+
 def get_occupation(bachelortv, hometown)
   match = contestants(bachelortv).find do |contestant|
     contestant["hometown"] = hometown
   end
 match["occupation"]
 end
+
 # def get_average_age_for_season(data, season)
 #   bachelortv.each do |season_number, contestant_data|
 #     if season == season_number
@@ -46,6 +52,7 @@ end
 #     end
 #   end
 # end
+
 def get_average_age_for_season(bachelortv, season)
   bachelortv.each do |season_number, contestant_data|
     if season == season_number
